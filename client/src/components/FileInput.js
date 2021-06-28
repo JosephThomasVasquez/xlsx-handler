216c907c0a45db56ed78e77b1a55782341f4e8ce
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-27 20:13:12 -0700

    Added some styling

--- client/src/components/FileInput.js
+++ client/src/components/FileInput.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const FileInput = () => {
     return (
-        <div>
-            <form action="">
+        <div className="container">
+            <form action="" className="form">
                 <label htmlFor="file">File</label>
-                <input type="text" name="file" id="file" />
-                <button type="submit">Browse</button>
+                <input type="file" name="file" id="file" className="file-input" />
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
