55835f0fb0374a138cc60db3374a6fc907a53f78
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-27 20:20:00 -0700

    Added file ahndler function

--- client/src/components/FileInput.js
+++ client/src/components/FileInput.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const FileInput = () => {
-    return (
-        <div className="container">
-            <form action="" className="form">
-                <label htmlFor="file">File</label>
-                <input type="file" name="file" id="file" className="file-input" />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-}
+  const [file, setFile] = useState("");
+  const handleFile = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    console.log(file)
+  };
+  return (
+    <div className="container">
+      <form action="" className="form">
+        <label htmlFor="file">File</label>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          className="file-input"
+          onChange={handleFile}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default FileInput
+export default FileInput;
