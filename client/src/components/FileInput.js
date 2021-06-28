0e7bfecf4c1e6c54f5d076d98a7736c86ddcd3ec
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-27 20:36:11 -0700

    Setup filereader and buffer config

--- client/src/components/FileInput.js
+++ client/src/components/FileInput.js
@@ -1,12 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const FileInput = () => {
   const [file, setFile] = useState("");
   const handleFile = (e) => {
     e.preventDefault();
-    const file = e.target.files[0];
-    console.log(file)
+
+    const fileData = e.target.files[0];
+
+    // Set file sate
+    setFile(fileData);
+
+    if (fileData) {
+      console.log(file);
+    }
+
+    fileHandler(fileData);
   };
+
+  useEffect(() => {
+    if (file) {
+      console.log('File loaded', file);
+    }
+  }, []);
+
+  const fileHandler = async (file) => {
+
+    // Create FileReader and read as Array Buffer
+    const fileReader = new FileReader();
+    await fileReader.readAsArrayBuffer(file);
+
+    // When file is loaded create buffer
+    fileReader.onload = (e) => {
+      const bufferArray = e.target.result;
+      console.log("Buffer Array", bufferArray);
+    };
+
+  };
+
   return (
     <div className="container">
       <form action="" className="form">
