13619c1600a377d3775591f748d0a38cb186b66a
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-27 20:57:13 -0700

    Excel now converts sheet to json

--- client/src/components/FileInput.js
+++ client/src/components/FileInput.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import * as xlsx from "xlsx";
 
 const FileInput = () => {
   const [file, setFile] = useState("");
@@ -19,22 +20,48 @@ const FileInput = () => {
 
   useEffect(() => {
     if (file) {
-      console.log('File loaded', file);
+      console.log("File loaded", file);
     }
   }, []);
 
-  const fileHandler = async (file) => {
+  const fileHandler = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      // Create FileReader and read as Array Buffer
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
 
-    // Create FileReader and read as Array Buffer
-    const fileReader = new FileReader();
-    await fileReader.readAsArrayBuffer(file);
+      // When file is loaded create buffer
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        console.log("Buffer Array", bufferArray);
 
-    // When file is loaded create buffer
-    fileReader.onload = (e) => {
-      const bufferArray = e.target.result;
-      console.log("Buffer Array", bufferArray);
-    };
+        // XLSX AREA -----------------------------------------------------------------------------------------
+        // Get Workbook
+        const workBook = xlsx.read(bufferArray, { type: "buffer" });
+        console.log("workBook: ", workBook);
 
+        // Get Sheet Name
+        const workSheet = workBook.SheetNames[0];
+        console.log("workSheet: ", workSheet);
+
+        // Sheet
+        const data = workBook.Sheets[workSheet];
+        console.log("sheet: ", data);
+
+        // Data
+        const sheetData = xlsx.utils.sheet_to_json(data);
+
+        resolve(sheetData);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d);
+    });
   };
 
   return (
