066b16177ec6e0ee6e785e3afbc53f7799dbc39f
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-28 20:52:47 -0700

    Added table component

---
+++ client/src/components/TableComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const TableComponent = () => {
+  return (
+    <div>
+      <table className="table-view">
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default TableComponent;
