561355769b55cb734aa7d44b7214a19051238f02
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-06-29 20:06:30 -0700

    Added alert message component to display success and error messages to user

---
+++ client/src/components/AlertMessageComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AlertMessageComponent = ({ message }) => {
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default AlertMessageComponent;
